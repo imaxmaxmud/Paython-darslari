475f070ec202fd387fb07975adde1e0c63d65d24
Mahmudjon <imaxmaxmud@gmail.com>
2025-02-28 19:08:13 +0500

    Create lugat asosiy 2.py
    lugatga yangi qoshildi

---
+++ lugat asosiy 2.py
@@ -0,0 +1,77 @@
+# talaba_0={
+    
+#     'ism':'Mahmudjon',
+#     'kurs':4,
+#     'yil':1991,
+#     'millati':'o`zbek',
+#     'yonalishi':'ax'    
+    
+#     }
+
+# for i in talaba_0:
+#     print(f"kalit:{i}")
+#     # print(f"qiymat:{j}\n")
+
+# telefonlar={
+# 'ali':'Iphone 15 Pro',
+# 'vali':'Redmi',
+# 'sanjar':'samsung a25',
+# 'ilyos':'huvai'
+# }
+# i=0
+# # print(telefonlar.items())
+# for kalit,qiymat in telefonlar.items():
+#     i=i+1
+#     print(f"\n {kalit} ning telefoni - {qiymat} ")
+
+#  .keys()
+# mahsulotlar={
+#     'olma':10000,
+#     'behi':6000,
+#     'nok':8000,
+#     'shaftoli':9000    
+#     }
+# # print(mahsulotlar.items())
+# # print(mahsulotlar.keys())
+# # for mahsulot in mahsulotlar.keys():
+# #     print(mahsulot.title())
+# bozorlik=['anor','olma','behi','shaftoli','nok']
+# for mahsulot in mahsulotlar:
+#     if mahsulot in bozorlik:
+#         print(f"{mahsulot.title()} {mahsulotlar[mahsulot]} so`m")
+# for buyum in bozorlik:
+#     if buyum not in mahsulotlar:
+#         print(f"Iltimos do`koningizga {buyum} ham olib keling")
+
+# mahsulotlar={
+#     'olma':5000,
+#     'banan':12000,
+#     'nok':12000,
+#     'orik':16000,
+#     'qovun':24000,
+#     'tarvuz':30000,
+#     'shaftoli':31000
+#     }
+# # bozorlik=['olma','banan','nok','orik','qovun','olcha','gilos']
+# for order in bozorlik:
+#     if order in mahsulotlar:
+#         print(f"{order} ning narhi {mahsulotlar[order]} so`m")
+#     else:
+#         print(f" Iltimos do`koningizga {order} ham olib keling")
+
+# for mahsulot in sorted(mahsulotlar):
+#     print(mahsulot.title())
+# for i in mahsulotlar.items():
+#     print(i)
+
+# quyidagida takrorlanadiganlarini o`chiradi va 1 marta chiqaradi  
+# for mahsulot in set(mahsulotlar.values()):
+#     print(mahsulot)
+
+#  Quyidagi holatda takorolanadiganalrini o`chirib tashlaydi
+# toy={'ball','car','lamp','ball'}
+# print(type(toy))
+# print(toy)
+
+
+
