f8447273241973ba34769475bc0cdc9e1bffbdf8
Mahmudjon <imaxmaxmud@gmail.com>
2025-04-21 10:52:37 +0500

    Update class_2.py

--- class_2.py
+++ class_2.py
@@ -5,14 +5,45 @@ class Talaba:
         self.fam=fam
         self.tyil=tyil 
         self.bosqich=1
-    def talaba_info(self):
-        return f"Ismi {self.ism} familiyasi {self.fam} t yili {self.tyil}"
     def get_name(self):
         return self.ism
     def get_surname(self):
         return self.fam
     def millat(self,millat):
         return self.millat
-    
-talaba1=Talaba('Mahmudjon','Sadikov',1991)
-print(talaba1.talaba_info())
+    def bosqichni_yangilash(self,yangi_bosqich):
+        self.bosqich=yangi_bosqich
+    def fam_yangilash(self, yangi_fam):
+        self.fam=yangi_fam 
+    def info_print(self):
+        return f"Talaba {self.ism} {self.fam} tugulgan yili {self.tyil} bosqich: {self.bosqich}"
+class Fan:
+    " Fanga oid klass yaratish"
+    def __init__(self,nomi):
+        self.nomi=nomi
+        self.talabalar_soni=0
+        self.talabalar=[]
+    def add_students(self,talaba):
+        self.talabalar.append(talaba)
+        self.talabalar_soni+=1
+    def get_students(self):
+        "Fanga yozilgan talabalar haqida ma`lumot"
+        return [ talaba.info_print() for talaba in self.talabalar   ]
+    def students_number(self):
+        " Fanga yozilgan talabalar sonini ko`rish"
+        x=self.talabalar_soni
+        return x
+          
+student1=Talaba('Mahmudjon','Sadikov',1991)
+# print(student1.info_print())
+
+matematika=Fan("Oliy matematika")
+matematika.add_students(student1)
+student2=Talaba("Hasan",'Aminboyev',2003)
+student3=Talaba('husan','Aminboyev',2003)
+matematika.add_students(student2)
+matematika.add_students(student3)
+
+
+
+
