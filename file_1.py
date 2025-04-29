c89cec65e714bd5f10ad6de19b4ba5303403fd45
Mahmudjon <imaxmaxmud@gmail.com>
2025-04-29 14:04:18 +0500

    file_1

---
+++ file_1.py
@@ -0,0 +1,15 @@
+# file=open('C:/pyt.txt')
+# och=file.read()
+# print(och)
+# file.close()
+
+with open('C:/pyt.txt') as file:
+    pi=file.read()
+    print(pi)
+    
+pi=pi.rstrip()
+pi=pi.repla
+print(pi)
+
+
+
