30304693a56d1684aac0c6532437202bc56e0c6f
Mahmudjon <imaxmaxmud@gmail.com>
2025-05-02 11:40:00 +0500

    json

--- json_1.py
+++ json_1.py
@@ -6,6 +6,75 @@ import json
 # x_json=json.dumps(x)
 # print(x_json)
 
-ism='Mahmudjon'
-ism_json=json.dumps(ism)
-print(ism_json)
+
+
+
+talaba={
+        'ism':"Sadikov mahmudjon",
+        'manzil':'Xorazm viloyati Yangibozor tumani',
+        'telefoni':'+998944048822',
+        'tyil':' Tug`ulgan yili',
+        'Dasturlash tillari':['C++','Java','Python']     
+        
+        }
+
+talaba_json=json.dumps(talaba)
+talaba2=json.loads(talaba_json)
+
+
+
+
+
+
+
+
+
+talaba_json=json.dumps(talaba)
+
+
+with open('C:/fff.txt','a') as f:
+    json.dump(talaba,f)
+   
+    
+
+
+
+
+
+talaba2=json.loads(talaba_json)
+
+numbers=[]
+i=0
+while i<=10:
+        numbers.append(i)
+        i+=1
+        
+
+print(numbers)
+
+numbers_json=json.dumps(numbers)
+
+
+numbers_2_json=json.loads(numbers_json)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
